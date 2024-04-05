d8a00db58a09e9f0191e7405919db27eca15f4bd
ki52 <lesha.sinitsin@mail.ru>
2024-04-05 18:27:21 +0300

    save string unique

--- string/isUnique.ts
+++ string/isUnique.ts
@@ -9,13 +9,24 @@ console.log(isUnique('abcadef')); // -> false
 //--------------------------------------------------------------------------2 вариант решения
 function isUnique2(string: string) {
   const map = new Map();
+  let isUnique = true;
   for (let index = 0; index < string.length; index++) {
     const element = string[index];
     if (map.has(element)) {
+      map.set(element, map.get(element) + 1);
     } else {
       map.set(element, 1);
     }
   }
+  for (let val of map.values()) {
+    if (isUnique) {
+      if (val != 1) {
+        isUnique = false;
+      }
+    }
+  }
+
+  return isUnique;
 }
 console.log(isUnique2('abcdef')); // -> true
 console.log(isUnique2('1234567')); // -> true
